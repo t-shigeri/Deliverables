8925bfb340862210da88021968e1cbd4dc9cfa52
Enta Shigeri <fko2447037@stu.o-hara.ac.jp>
2025-07-07 11:25:42 +0900

    portfolio-作品をjsonで管理

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -5,13 +5,11 @@ export default function Contact() {
     const [submitted, setSubmitted] = useState(false);
     const [error, setError] = useState('');
 
-    // 送信処理をフックする
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError('');
         const formData = new FormData(e.target);
 
-        // Formspreeのエンドポイントにfetch送信
         const response = await fetch('https://formspree.io/f/manorjzd', {
             method: 'POST',
             body: formData,
@@ -34,7 +32,7 @@ export default function Contact() {
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.6 }}
             >
-                お問い合わせ
+                CONTACT
             </motion.h2>
 
             {!submitted ? (
@@ -60,7 +58,7 @@ export default function Contact() {
                     animate={{ opacity: 1 }}
                     transition={{ duration: 0.5 }}
                 >
-                    お問い合わせありがとうございました！
+                    Thnak you!
                 </motion.p>
             )}
         </section>
--- my-portfolio/src/pages/Works.jsx
+++ my-portfolio/src/pages/Works.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import WorkCard from '../components/WorkCard';
 import '../style/works.css';
-
+import works from '../data/works.json';
 
 function Works() {
-  const works = [
-    {
-      image: '/assets/portfolioimage.png',
-      title: 'ポートフォリオサイト',
-      description: 'ReactとViteで制作。私のポートフォリオ。',
-      link: 'https://example.com/portfolio',
-    },
-    {
-      image: '/assets/pc.png',
-      title: 'トイレ検索アプリ',
-      description: 'React + Map API + Tailwindで開発。近場のトイレを検索することができる。',
-      link: 'https://example.com/toilet-app',
-    },
-  ];
-
   return (
     <div className="works-wrapper">
-      <h2 className="works-title">作品一覧</h2>
+      <h2 className="works-title">WORKS</h2>
       <div className="works-list">
         {works.map((work, index) => (
           <WorkCard
